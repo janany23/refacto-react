37b903921db26e47718238e319e5ae11e32ea273
Janany Perinparajah <janany.perinparajah@systrangroup.com>
2022-06-13 21:03:52 +0200

    refactor(DomainFilter): refactor to a stateless component and use Set type for the list of options

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -1,58 +1,31 @@
 import React from 'react';
 import FilterComponent from "../Filter/Filter.component";
 
-interface State {
-  countries: string[],
-  classifications: string[],
-  subClassifications: string[]
-}
-
 interface Props {
   domains: string[]
 }
 
-class DomainFilter extends React.Component<Props, State> {
-  state: State = {
-    countries: [],
-    classifications: [],
-    subClassifications: []
-  }
-  componentDidMount() {
-    const { domains } = this.props;
+const DomainFilter = ({ domains }: Props) => {
+  const countries: Set<string> = new Set();
+  const classifications: Set<string> = new Set();
+  const subClassifications: Set<string> = new Set();
 
-    for(let i = 0; i < domains.length; i++) {
-      if (this.state.countries.indexOf(domains[i].substring(0,2)) <= 0) {
-        this.state.countries.push(domains[i].substring(0,2))
-      }
-      this.state.classifications.push(domains[i].substring(3,5));
-      let flag = false;
-      for(let j = 0; j < this.state.subClassifications.length; j++) {
-        if (this.state.subClassifications[j] === domains[i].substring(6)) {
-          flag = true
-          break;
-        }
-      }
-      if (!flag) {
-        this.state.subClassifications.push(domains[i].substring(6));
-      }
-    }
+  for(let i = 0; i < domains.length; i++) {
+    // countries
+    countries.add(domains[i].substring(0, 2));
 
-    this.setState({
-      ...this.state,
-      classifications: this.state.classifications.filter((e, i, l) => l.indexOf(e) === i),
-    })
-    this.forceUpdate()
+    // classifications
+    classifications.add(domains[i].substring(3, 5));
+
+    // subClassifications
+    subClassifications.add(domains[i].substring(6));
   }
 
-  render() {
-    const {countries = [], classifications = [], subClassifications = []} = this.state;
-
-    return (<>
-      <FilterComponent name="countries"  options={countries} />
-      <FilterComponent name="classifications"  options={classifications} />
-      <FilterComponent name="subClassifications"  options={subClassifications} />
-    </>)
-  }
+  return (<>
+    <FilterComponent name="countries"  options={Array.from(countries)} />
+    <FilterComponent name="classifications" options={Array.from(classifications)} />
+    <FilterComponent name="subClassifications" options={Array.from(subClassifications)} />
+  </>)
 }
 
 export default DomainFilter
